daeadd78eec46c209549acd2d439dca0d96613c3
dimastpx <dimas30072007@gmail.com>
2024-02-09 19:26:35 +1000

    window in process

--- window.py
+++ window.py
@@ -3,17 +3,34 @@ import tkinter as tk
 from tkinter import ttk
 
 window = tk.Tk()
-window.geometry("600x600")
+window.geometry("800x600")
 frame = ttk.Frame(window, width=500, height=500)
 frame.grid(sticky="nsew")
 
+style = ttk.Style()
+style.configure('my.TButton', font=('Helvetica', 20))
+
+class TicButton():
+    def __init__(self, corx = int, cory = int):
+        self.x = corx + 1
+        self.y = cory + 1
+        self.button = ttk.Button(frame, text=" ", style="my.TButton", command=self.button_click)
+        self.button.grid(row=self.y, column=self.x, ipady=50, ipadx=10, sticky="nsew")
+    def button_click(self):
+        print(self.x, self.y)
 
 
+# цикл для создания списка
+board = []
+row = []
 for i in range(3):
     for j in range(3):
-        button = ttk.Button(frame, text=j)
+        row.append(TicButton(j, i).y)
+    board.append(row)
+    row = []
 
-        button.grid(row=i, column=j, sticky="nsew")
+for i in board:
+    print(i)
 
 
 window.mainloop()
