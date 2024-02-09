7221475cbcf6aae7bff8e57f4ae9bd07b8196c06
dimastpx <dimas30072007@gmail.com>
2024-02-09 21:38:43 +1000

    window in process

--- window.py
+++ window.py
@@ -12,14 +12,38 @@ style.configure('my.TButton', font=('Helvetica', 20))
 
 class TicButton():
     def __init__(self, corx = int, cory = int):
-        self.x = corx + 1
-        self.y = cory + 1
+        self.x = corx
+        self.y = cory
         self.button = ttk.Button(frame, text=" ", style="my.TButton", command=self.button_click)
         self.button.grid(row=self.y, column=self.x, ipady=50, ipadx=10, sticky="nsew")
+
+    # Событие нажатия на кнопку
     def button_click(self):
         if not self.button["text"] in ("X","0"):
             # Добавить выбор знака
             self.button["text"] = "X"
+            self.win_test()
+            self.computer()
+
+    # Компьютер ходит
+    def computer(self):
+        global board
+        compx = randint(0,2)
+        compy = randint(0,2)
+        if not board[compy][compx].button["text"] in ("X","0"):
+            board[compy][compx].button["text"] = "0"
+            self.win_test()
+        else:
+            self.computer()
+
+# Проверка (не трогать)
+    def win_test(self):
+        global board, easy_board
+        for i in range(3):
+            for j in range(3):
+                easy_board[j][i] = board[j][i].button["text"]
+        print(easy_board)
+
 
 
 # Цикл для создания списка
@@ -28,12 +52,18 @@ board = []
 row = []
 for i in range(3):
     for j in range(3):
-        row.append(TicButton(j, i).y)
+        row.append(TicButton(j, i))
     board.append(row)
     row = []
 
-for i in board:
-    print(i)
+easy_board = []
+for i in range(3):
+    for j in range(3):
+        row.append(board[i][j])
+    easy_board.append(row)
+    row = []
+
+
 
 
 window.mainloop()
